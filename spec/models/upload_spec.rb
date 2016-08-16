56e93454e613a0a602e14db48328f9547319afcb
Guo Xiang Tan <tgx_world@hotmail.com>
2016-08-16 14:27:31 +0800

    Add specs for make sure jobs are enqueued.

---
+++ spec/models/upload_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+require 'sidekiq/testing'
+
+describe Upload do
+  let(:upload) { Fabricate(:upload) }
+
+  before do
+    @original_site_setting = SiteSetting.queue_jobs
+    SiteSetting.queue_jobs = true
+
+    DiscourseBackupUploadsToS3::Utils.expects(:backup_uploads_to_s3?)
+      .returns(true).at_least_once
+  end
+
+  after do
+    SiteSetting.queue_jobs = @original_site_setting
+  end
+
+  it "should enqueue a job to backup upload to S3" do
+    Sidekiq::Testing.fake! do
+      expect { upload }.to change { ::Jobs::BackupUploadToS3.jobs.size }.by(1)
+    end
+  end
+
+  it "should enqueue a job to remove upload from s3 when upload is destroyed" do
+    Sidekiq::Testing.fake! do
+      expect { upload.destroy }.to change { ::Jobs::RemoveUploadFromS3.jobs.size }.by(1)
+    end
+  end
+end
