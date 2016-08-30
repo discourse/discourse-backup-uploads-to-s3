cba03a3ab54455987c1fbd7afb5685b74c7ae574
Guo Xiang Tan <tgx_world@hotmail.com>
2016-08-30 17:23:36 +0800

    Remove warning about order and limit being ignored when using `find_each`.

--- app/jobs/scheduled/backfill_uploads_backup.rb
+++ app/jobs/scheduled/backfill_uploads_backup.rb
@@ -4,7 +4,7 @@ module Jobs
 
     def execute(args)
       return unless DiscourseBackupUploadsToS3::Utils.backup_uploads_to_s3?
-      Upload.not_backuped.limit(5000).find_each { |upload| upload.backup_to_s3 }
+      Upload.not_backuped.limit(5000).each { |upload| upload.backup_to_s3 }
     end
   end
 end
