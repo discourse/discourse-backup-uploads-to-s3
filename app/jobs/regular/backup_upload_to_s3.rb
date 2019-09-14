7c85e04c901d34a85f1ee60678f69c2b1c15851a
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-09-15 09:52:41 +1000

    FIX: Use top-level namespace for base classes

--- app/jobs/regular/backup_upload_to_s3.rb
+++ app/jobs/regular/backup_upload_to_s3.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class BackupUploadToS3 < Jobs::Base
+  class BackupUploadToS3 < ::Jobs::Base
     sidekiq_options queue: 'low'
 
     def execute(args)
--- app/jobs/regular/remove_upload_from_s3.rb
+++ app/jobs/regular/remove_upload_from_s3.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Jobs
-  class RemoveUploadFromS3 < Jobs::Base
+  class RemoveUploadFromS3 < ::Jobs::Base
     sidekiq_options queue: 'low'
 
     def execute(args)
